7c84a0096da3783527001c03deaafce95794a1bd
MaiCongPhuoc <maiphuoc244@gmail.com>
2022-10-06 00:49:24 +0700

    phuoc-dev 10/06/2022

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Dashboard from './dashboard/pages/Dashboard';
+
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/css/sb-admin-2.min.css';
+
+import $ from '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/jquery.easing/jquery.easing.min.js';
+// import '../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.min.js';
+import '../node_modules/chart.js/dist/Chart.min.js';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Dashboard />
         </div>
     );
 }
