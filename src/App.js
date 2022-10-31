f94c87292ef04113725c605fab6653841da0dd6f
TranNgocTrung <milotran1496@gmail.com>
2022-10-31 18:05:44 +0700

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+// import logo from './logo.svg';
+import Dashboard from './dashboard/pages/Dashboard';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/bootstrap-5.2.0-dist/css/bootstrap.min.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
 import './assets/css/sb-admin-2.min.css';
+// import '../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.min.js';
+
 import Product from './products/Product';
 import ListProduct from './dashboard/pages/ListProduct';
 import ListAccount from './dashboard/pages/ListAccount';
